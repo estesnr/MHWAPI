290f325a269231e0c575de88a2712a54fd6e4b96
Nate Estes <97855282+estesnr@users.noreply.github.com>
2022-06-04 12:37:22 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -7,27 +7,56 @@ const monsters = {
 'anjanath' : {
     "name" : 'Anjanath',
     "type" : 'Brute Wyvern',
-    "weakness" : ["Water***", 'Thunder**', 'Ice**']
+    "weakness" : ["Water***", 'Thunder**', 'Ice**'],
+    'img' : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fmonsterhunterespanol%2Fimages%2F2%2F29%2FMHW-Render_Anjanath_001.png%2Frevision%2Flatest%3Fcb%3D20171118105044%26path-prefix%3Des&f=1&nofb=1'
 },
 'tobi-kadachi' : {
     'name' : "Tobi-Kadachi",
     'type' : 'Fanged Wyvern',
-    'weakness' : ['Water***', 'Fire**', 'Ice**']
+    'weakness' : ['Water***', 'Fire**', 'Ice**'],
+    'img' : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fmonsterhunterespanol%2Fimages%2Ff%2Ff9%2FMHW-Render_Tobi-Kadachi.png%2Frevision%2Flatest%2Fscale-to-width-down%2F640%3Fcb%3D20171012202936%26path-prefix%3Des&f=1&nofb=1'
 },
 'rathian' : {
     'name' : 'Rathian',
     'type' : 'Flying Wyvern',
-    'weakness' : ['Dragon***', 'Thunder**']
+    'weakness' : ['Dragon***', 'Thunder**'],
+    'img' : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fmonsterhunter%2Fimages%2F8%2F84%2FMHW-Rathian_Render_001.png%2Frevision%2Flatest%3Fcb%3D20180201020034&f=1&nofb=1'
 },
 'rathalos' : {
     'name' : 'Rathalos',
     'type' : 'Flying Wyvern',
-    'weakness' : ['Dragon***', 'Thunder**', 'Water*', 'Ice*']
+    'weakness' : ['Dragon***', 'Thunder**', 'Water*', 'Ice*'],
+    'img' : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fmonsterhunterespanol%2Fimages%2F3%2F34%2FMHW-Render_Rathalos_001.png%2Frevision%2Flatest%3Fcb%3D20171118104211%26path-prefix%3Des&f=1&nofb=1'
+},
+'deviljho' : {
+    'name' : 'Deviljho',
+    'type' : 'Brute Wyvern',
+    'weakness' : ['Fire**', 'Water**', 'Thunder***', 'Ice*', 'Dragon***'],
+    'img' : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fmonsterhunter%2Fimages%2Fc%2Fcb%2FMHW-Deviljho_Render_001.png%2Frevision%2Flatest%3Fcb%3D20180105132807&f=1&nofb=1'
+},
+'dodogama' : {
+    'name' : 'Dodogama',
+    'type' : 'Fanged Wyvern',
+    'weakness' : ['Thunder***', 'Water**', 'Ice**', 'Dragon*'],
+    'img' : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fmonsterhunterespanol%2Fimages%2F7%2F7c%2FMHW-Render_Dodogama.png%2Frevision%2Flatest%3Fcb%3D20180112173353%26path-prefix%3Des&f=1&nofb=1'
+},
+'diablos' : {
+    'name' : 'Diablos',
+    'type' : 'Flying Wyvern',
+    'weakness' : ['Ice***', 'Dragon**', 'Water**', 'Thunder**'],
+    'img' : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fmogapedia%2Fimages%2F7%2F75%2FMHW-Diablos_Render_001.png%2Frevision%2Flatest%3Fcb%3D20180201140345%26path-prefix%3Dfr&f=1&nofb=1'
+},
+'legiana' : {
+    'name' : 'Legiana',
+    'type' : 'Flying Wyvern',
+    'weakness' : ['Thunder***', 'Fire**'],
+    'img' : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fmonsterhunterespanol%2Fimages%2F1%2F1f%2FMHW-Render_Legiana.png%2Frevision%2Flatest%3Fcb%3D20171112153522%26path-prefix%3Des&f=1&nofb=1'
 },
 'unknown' : {
     'name' : 'unknown',
     'type' : 'unknown',
-    'weakness' : 'unknown'
+    'weakness' : 'unknown',
+    'img' : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.downloadclipart.net%2Flarge%2Fcthulhu-png-transparent-background.png&f=1&nofb=1'
 }
 }
 
@@ -42,7 +71,7 @@ app.get('/api', (req, res) => {
 })
 
 app.get('/api/:monst', (req, res) => {
-    const monstName = request.params.name.toLowerCase()
+    const monstName = req.params.monst.toLowerCase()
     if(monsters[monstName]) {
         res.json(monsters[monstName])
     }
